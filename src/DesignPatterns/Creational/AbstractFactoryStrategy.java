3d9b4ac61b540ac6203e709736049ce408b74536
duydoan <duy.doan@tyme.com>
2025-04-13 13:58:19 +0700

    Abstract Factory with Strategy Pattern

---
+++ src/DesignPatterns/Creational/AbstractFactoryStrategy.java
@@ -0,0 +1,84 @@
+package DesignPatterns.Creational;
+
+import static DesignPatterns.Creational.PaymentType.CREDITCARD;
+import static DesignPatterns.Creational.PaymentType.MOMO;
+import static DesignPatterns.Creational.PaymentType.PAYPAL;
+import static DesignPatterns.Creational.PaymentType.ZALOPAY;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+// Abstract Product Strategy
+interface PaymentStrategy {
+   void pay(double amount);
+}
+
+enum PaymentType {
+   MOMO, ZALOPAY, PAYPAL, CREDITCARD;
+}
+// Concrete Product
+class MomoStrategy implements PaymentStrategy {
+   public void pay(double amount) {
+      System.out.println("Thanh toán " + amount + " bằng Momo.");
+   }
+}
+// Concrete Product
+class ZaloPayStrategy implements PaymentStrategy {
+   public void pay(double amount) {
+      System.out.println("Thanh toán " + amount + " bằng ZaloPay.");
+   }
+}
+// Concrete Product
+class PayPalStrategy implements PaymentStrategy {
+   public void pay(double amount) {
+      System.out.println("Pay $" + amount + " via PayPal.");
+   }
+}
+// Concrete Product
+class CreditCardStrategy implements PaymentStrategy {
+   public void pay(double amount) {
+      System.out.println("Pay $" + amount + " via CreditCard.");
+   }
+}
+
+class PaymentStrategyManager {
+
+   Map<PaymentType, Supplier<PaymentStrategy>> strategies = Map.of(
+         MOMO, MomoStrategy::new,
+         ZALOPAY, ZaloPayStrategy::new,
+         PAYPAL, PayPalStrategy::new,
+         CREDITCARD, CreditCardStrategy::new
+   );
+
+   PaymentStrategy getStrategy(PaymentType paymentType) {
+      return strategies.get(paymentType).get();
+   }
+
+   void showOptions() {
+      System.out.println("Chọn phương thức thanh toán:");
+      for (PaymentType paymentType : strategies.keySet()) {
+         System.out.println("- " + paymentType.name());
+      }
+   }
+}
+
+public class AbstractFactoryStrategy {
+
+   public static void main(String[] args) {
+      PaymentStrategyManager manager = new PaymentStrategyManager();
+      manager.showOptions();
+      PaymentStrategy paymentStrategy;
+
+      paymentStrategy = manager.getStrategy(ZALOPAY);
+      paymentStrategy.pay(10);
+
+      paymentStrategy = manager.getStrategy(MOMO);
+      paymentStrategy.pay(20);
+
+      paymentStrategy = manager.getStrategy(CREDITCARD);
+      paymentStrategy.pay(30);
+
+      paymentStrategy = manager.getStrategy(PAYPAL);
+      paymentStrategy.pay(40);
+   }
+}
