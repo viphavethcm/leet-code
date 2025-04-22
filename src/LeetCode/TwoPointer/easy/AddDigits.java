d151d68ddf912abb309a1c57436b058e3075a2c3
duydoan <duy.doan@tyme.com>
2025-04-22 11:48:12 +0700

    AddDigits

---
+++ src/LeetCode/TwoPointer/easy/AddDigits.java
@@ -0,0 +1,26 @@
+package LeetCode.TwoPointer.easy;
+
+import java.util.Scanner;
+
+public class AddDigits {
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int num = sc.nextInt();
+      System.out.println(addDigits(num));
+   }
+
+   public static int addDigits(int num) {
+      int digitalRoot = 0;
+      while (num > 0) {
+         digitalRoot += num % 10;
+         num = num / 10;
+
+         if (num == 0 && digitalRoot > 9) {
+            num = digitalRoot;
+            digitalRoot = 0;
+         }
+      }
+      return digitalRoot;
+   }
+}
