32b3b24763055a0d2d0a51a223c7439a45daf42b
b4u-dev <duy.doan@tyme.com>
2025-08-16 12:16:11 +0700

    GreatestStringCommon

---
+++ src/LeetCode/String/easy/GreatestStringCommon.java
@@ -0,0 +1,34 @@
+package LeetCode.String.easy;
+
+import java.util.Scanner;
+
+public class GreatestStringCommon {
+
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      String str1 = in.nextLine();
+      String str2 = in.nextLine();
+      System.out.println(gcdOfStrings(str1, str2));
+   }
+
+   public static boolean valid(String str1, String str2, int k) {
+      int len1 = str1.length(), len2 = str2.length();
+      if (len1 % k > 0 || len2 % k > 0) {
+         return false;
+      } else {
+         String base = str1.substring(0, k);
+         return str1.replace(base, "").isEmpty() && str2.replace(base, "").isEmpty();
+      }
+   }
+
+
+   public static String gcdOfStrings(String str1, String str2) {
+      int len1 = str1.length(), len2 = str2.length();
+      for (int i = Math.min(len1, len2); i >= 1; --i) {
+         if (valid(str1, str2, i)) {
+            return str1.substring(0, i);
+         }
+      }
+      return "";
+   }
+}
