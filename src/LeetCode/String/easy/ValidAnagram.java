f9ae1fb9be3c1a7a0ff90085ae2b51932e6947d2
b4u-dev <duy.doan@tyme.com>
2025-10-06 18:06:36 +0700

    ValidAnagram

---
+++ src/LeetCode/String/easy/ValidAnagram.java
@@ -0,0 +1,46 @@
+package LeetCode.String.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+
+   public static void main(String[] args) {
+      String s = "anagram", t = "nagaram";
+      isAnagram(s, t);
+   }
+
+   // Sorting
+   public  static boolean isAnagram(String s, String t) {
+      if (s.length() != t.length()) return false;
+      char[] str1 = s.toCharArray();
+      char[] str2 = t.toCharArray();
+      Arrays.sort(str1);
+      Arrays.sort(str2);
+      return Arrays.equals(str1, str2);
+   }
+
+   // Hash Map
+//   public static boolean isAnagram(String s, String t) {
+//      Map<Character, Integer> map = new HashMap<>();
+//      for (Character c: s.toCharArray()) {
+//         if (!map.containsKey(c)) {
+//            map.putIfAbsent(c, 1);
+//         }
+//         else {
+//            map.computeIfPresent(c, (k, v) -> v + 1);
+//         }
+//      }
+//      for (Character c: t.toCharArray()) {
+//         if (!map.containsKey(c)) {
+//            return false;
+//         }
+//         map.computeIfPresent(c, (k, v) -> v - 1);
+//         if (map.get(c) == 0) {
+//            map.remove(c);
+//         }
+//      }
+//      return map.isEmpty();
+//   }
+}
