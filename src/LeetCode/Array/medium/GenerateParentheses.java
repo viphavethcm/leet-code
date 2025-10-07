cfd953e3075745c8a698cba343683296e9a1d6b0
b4u-dev <duy.doan@tyme.com>
2025-10-07 23:07:39 +0700

    GenerateParentheses

---
+++ src/LeetCode/Array/medium/GenerateParentheses.java
@@ -0,0 +1,33 @@
+package LeetCode.Array.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+
+   public static void main(String[] args) {
+      List<String> ans = generateParenthesis(3);
+   }
+   public static List<String> generateParenthesis(int n) {
+      List<String> res = new ArrayList<>();
+      backtrack(res, n, new StringBuilder(), 0, 0);
+      return res;
+   }
+
+   public static void backtrack(List<String> ans, int n, StringBuilder currString, int left, int right) {
+      if (currString.length() >= 2*n) {
+         ans.add(currString.toString());
+         return;
+      }
+      if (left < n) {
+         currString.append("(");
+         backtrack(ans, n, currString, left + 1, right);
+         currString.deleteCharAt(currString.length() - 1);
+      }
+      if (right < left) {
+         currString.append(")");
+         backtrack(ans, n, currString, left, right + 1);
+         currString.deleteCharAt(currString.length() - 1);
+      }
+   }
+}
