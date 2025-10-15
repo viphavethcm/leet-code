dc4280f39d50ff0e03c2292adcdd851f927fce2a
b4u-dev <duy.doan@tyme.com>
2025-10-15 15:42:00 +0700

    ValidAnagram

--- src/LeetCode/String/easy/ValidAnagram.java
+++ src/LeetCode/String/easy/ValidAnagram.java
@@ -12,13 +12,30 @@ public class ValidAnagram {
    }
 
    // Sorting
-   public  static boolean isAnagram(String s, String t) {
-      if (s.length() != t.length()) return false;
-      char[] str1 = s.toCharArray();
-      char[] str2 = t.toCharArray();
-      Arrays.sort(str1);
-      Arrays.sort(str2);
-      return Arrays.equals(str1, str2);
+//   public  static boolean isAnagram(String s, String t) {
+//      if (s.length() != t.length()) return false;
+//      char[] str1 = s.toCharArray();
+//      char[] str2 = t.toCharArray();
+//      Arrays.sort(str1);
+//      Arrays.sort(str2);
+//      return Arrays.equals(str1, str2);
+//   }
+
+   public static boolean isAnagram(String s, String t) {
+      if (s.length() != t.length()) {
+         return false;
+      }
+      int[] table = new int[26];
+      for (int i = 0; i < s.length(); i++) {
+         table[s.charAt(i) - 'a']++;
+      }
+      for (int i = 0; i < t.length(); i++) {
+         table[t.charAt(i) - 'a']--;
+         if (table[t.charAt(i) - 'a'] < 0) {
+            return false;
+         }
+      }
+      return true;
    }
 
    // Hash Map
